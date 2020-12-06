203f0c799768c168351a6c6d05a27753d0e1c22e
ArcAios26 <arcaios26@gmail.com>
2020-12-05 19:57:00 -0600

    Minor Refactoring

--- src/main/java/arcaios26/astraladditions/init/ModRecipes.java
+++ src/main/java/arcaios26/astraladditions/init/ModRecipes.java
@@ -1,9 +1,11 @@
-package arcaios26.astraladditions.crafting;
+package arcaios26.astraladditions.init;
+
+import arcaios26.astraladditions.crafting.AstralRingRecipe;
 
 import static hellfirepvp.astralsorcery.common.crafting.altar.AltarRecipeRegistry.cacheLocalRecipes;
 import static hellfirepvp.astralsorcery.common.crafting.altar.AltarRecipeRegistry.registerAltarRecipe;
 
-public class Recipes {
+public class ModRecipes {
     public static AstralRingRecipe rAstralRing;
 
     public static void initRecipes() {
--- src/main/java/arcaios26/astraladditions/proxy/CommonProxy.java
+++ src/main/java/arcaios26/astraladditions/proxy/CommonProxy.java
@@ -1,7 +1,6 @@
 package arcaios26.astraladditions.proxy;
 
-import arcaios26.astraladditions.crafting.AstralRingRecipe;
-import arcaios26.astraladditions.crafting.Recipes;
+import arcaios26.astraladditions.init.ModRecipes;
 import arcaios26.astraladditions.items.wearable.AstralRing;
 import arcaios26.astraladditions.network.PacketHandler;
 import net.minecraft.item.Item;
@@ -12,9 +11,6 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import static hellfirepvp.astralsorcery.common.crafting.altar.AltarRecipeRegistry.cacheLocalRecipes;
-import static hellfirepvp.astralsorcery.common.crafting.altar.AltarRecipeRegistry.registerAltarRecipe;
-
 @Mod.EventBusSubscriber
 public class CommonProxy {
     public void preInit(FMLPreInitializationEvent event) {
@@ -22,7 +18,7 @@ public class CommonProxy {
     }
 
     public void init(FMLInitializationEvent event) {
-        Recipes.initRecipes();
+        ModRecipes.initRecipes();
     }
 
     public void postInit(FMLPostInitializationEvent event) {
