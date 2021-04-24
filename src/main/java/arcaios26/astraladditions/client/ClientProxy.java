36c7023f055450b70a85d67f6d12aed07124babe
ArcAios26 <7105018+ArcAios26@users.noreply.github.com>
2021-04-23 22:22:50 -0500

    forgot to add reloadlisteners for AssetLibrary and AssetPreLoader

--- src/main/java/arcaios26/astraladditions/client/ClientProxy.java
+++ src/main/java/arcaios26/astraladditions/client/ClientProxy.java
@@ -1,6 +1,11 @@
 package arcaios26.astraladditions.client;
 
+import arcaios26.astraladditions.AstralAdditions;
+import arcaios26.astraladditions.client.resource.AssetLibrary;
+import arcaios26.astraladditions.client.resource.AssetPreLoader;
 import arcaios26.astraladditions.common.CommonProxy;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.IReloadableResourceManager;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
@@ -8,6 +13,12 @@ public class ClientProxy extends CommonProxy {
 
     @Override
     public void initialize() {
+        if (!AstralAdditions.isDoingDataGeneration()) {
+            IReloadableResourceManager resourceManager = (IReloadableResourceManager) Minecraft.getInstance().getResourceManager();
+            resourceManager.addReloadListener(AssetLibrary.INSTANCE);
+            resourceManager.addReloadListener(AssetPreLoader.INSTANCE);
+        }
+        
         super.initialize();
     }
 
