088a8d4f85a4ed90f8095efa1bf618735f7ca4a0
ArcAios26 <7105018+ArcAios26@users.noreply.github.com>
2021-04-25 01:48:59 -0500

    added localizations for walls, fences, journal cluster, and item group

--- src/main/java/arcaios26/astraladditions/datagen/data/AALanguageProvider.java
+++ src/main/java/arcaios26/astraladditions/datagen/data/AALanguageProvider.java
@@ -1,6 +1,7 @@
 package arcaios26.astraladditions.datagen.data;
 
 import arcaios26.astraladditions.AstralAdditions;
+import arcaios26.astraladditions.common.CommonProxy;
 import arcaios26.astraladditions.common.lib.BlocksAA;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.LanguageProvider;
@@ -13,6 +14,28 @@ public class AALanguageProvider extends LanguageProvider {
 
     @Override
     protected void addTranslations() {
+        //Item Groups
+        add(CommonProxy.ITEM_GROUP_AA.getGroupName().getString(), AstralAdditions.NAME);
+
+        //Blocks
         add(BlocksAA.BLACK_MARBLE_WALL.getTranslationKey(), "Sooty Marble Wall");
+        add(BlocksAA.BLACK_MARBLE_WALL_ARCH.getTranslationKey(), "Sooty Marble Arch Wall");
+        add(BlocksAA.BLACK_MARBLE_WALL_BRICKS.getTranslationKey(), "Sooty Marble Brick Wall");
+        add(BlocksAA.BLACK_MARBLE_WALL_RUNED.getTranslationKey(), "Sooty Runed Marble Wall");
+        add(BlocksAA.BLACK_MARBLE_WALL_CHISELED.getTranslationKey(), "Sooty Chiseled Marble Wall");
+        add(BlocksAA.BLACK_MARBLE_WALL_ENGRAVED.getTranslationKey(), "Sooty Engraved Marble Wall");
+        add(BlocksAA.MARBLE_WALL.getTranslationKey(), "Marble Wall");
+        add(BlocksAA.MARBLE_WALL_ARCH.getTranslationKey(), "Marble Arch Wall");
+        add(BlocksAA.MARBLE_WALL_BRICKS.getTranslationKey(), "Marble Brick Wall");
+        add(BlocksAA.MARBLE_WALL_RUNED.getTranslationKey(), "Runed Marble Wall");
+        add(BlocksAA.MARBLE_WALL_CHISELED.getTranslationKey(), "Chiseled Marble Wall");
+        add(BlocksAA.MARBLE_WALL_ENGRAVED.getTranslationKey(), "Engraved Marble Wall");
+        add(BlocksAA.INFUSED_WOOD_FENCE.getTranslationKey(), "Infused Wood Fence");
+        add(BlocksAA.INFUSED_WOOD_FENCE_GATE.getTranslationKey(), "Infused Wood Fence Gate");
+        add(BlocksAA.VIBRANT_INFUSED_WOOD_FENCE.getTranslationKey(), "Vibrant Infused Wood Fence");
+        add(BlocksAA.VIBRANT_INFUSED_WOOD_FENCE_GATE.getTranslationKey(), "Vibrant Infused Wood Fence Gate");
+
+        //Research Clusters
+        add("astralsorcery.journal.research.ascension", "Ascension");
     }
 }
